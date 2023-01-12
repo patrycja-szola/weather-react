d899bacf817fb6a49324315563f086b3054923a0
szyszka212 <szyszka212@gmail.com>
2023-01-12 22:59:21 +0100

    Added axios integration

---
+++ src/Weather.js
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios";
+import { Comment } from "react-loader-spinner";
+
+// npm start - tym rozpoczynam
+// Ctrl C - tym kończę
+// przed każdym załadowaniem nowej biblioteki, najlepiej zakończyć, potem załadować, wprowadzić zmiany w kodzie i wtedy dopiero rozpocząć od nowa
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+  }
+  let apikey = "1a503fb7a97ad8050479d85fae658043";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apikey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return (
+    <Comment
+      visible={true}
+      height="80"
+      width="80"
+      ariaLabel="comment-loading"
+      wrapperStyle={{}}
+      wrapperClass="comment-wrapper"
+      color="#fff"
+      backgroundColor="#F4442E"
+    />
+  );
+}
